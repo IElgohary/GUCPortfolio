7e027dd4e521c0717d50ec8977b6073885f4d6ac
Islam Elgohary <ielgohary@Islams-MacBook-Pro.local>
2017-03-09 19:39:56 +0200

    fixied static folder

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ setUpPassport();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(session({
